ee89f6a963de9387a90220992a3fdfa92b5a6dba
wzNaji <ludi2300@gmail.com>
2024-04-03 11:51:18 +0200

    Initial commit

---
+++ src/main/java/com/boefcity/wishlistv1/WishListV1Application.java
@@ -0,0 +1,13 @@
+package com.boefcity.wishlistv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/boefcity/wishlistv1/WishListV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boefcity.wishlistv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
