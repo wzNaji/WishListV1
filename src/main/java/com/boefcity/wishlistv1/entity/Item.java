922f32a655673715c413f9a7e3e93f6f2921d709
wzNaji <ludi2300@gmail.com>
2024-04-12 19:43:06 +0200

    Added: redirect att. messages in html forms.
    Todo: Delete functionality

--- src/main/java/com/boefcity/wishlistv1/entity/Item.java
+++ src/main/java/com/boefcity/wishlistv1/entity/Item.java
@@ -5,23 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name="items")
-
-
+@Data // Genererer boilerplate (getters, setters..)
+@NoArgsConstructor // Genererer en konstruktør uden argumenter
+@AllArgsConstructor // Genererer en konstruktør med alle argumenter
+@Entity // Enhed i JPA-kontekst
+@Table(name="items") // Mapper items entity/klasse til en tabel hvor name = "items" i databasen
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id //Angiver primærnøglen
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment ID/PK
     private int id;
+
+    @Column(nullable = false) // Gør kolonnen non-nullable
     private String name;
+
     private String description;
     private String link;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
+    @ManyToOne // Etablerer et Many-To-One forhold til User entity
+    @JoinColumn(name = "user_id") // Mapper til 'user_id' kolonnen i databasen
+    private User user; // Repræsenterer brugeren, der er forbundet med denne vare
 }
--- src/main/java/com/boefcity/wishlistv1/entity/User.java
+++ src/main/java/com/boefcity/wishlistv1/entity/User.java
@@ -21,5 +21,4 @@ public class User {
     @Column(nullable = false)
     private String userPassword;
 
-    private String userSession;
 }
