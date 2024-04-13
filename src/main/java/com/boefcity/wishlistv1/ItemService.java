289e3c0abd60dcf8235ad5cbc6a6674e426523a1
wzNaji <141943564+wzNaji@users.noreply.github.com>
2024-04-13 04:32:33 +0200

    minor

--- src/main/java/com/boefcity/wishlistv1/ItemService.java
+++ src/main/java/com/boefcity/wishlistv1/ItemService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface ItemService {
     void create (Item item);
     Optional<Item> findById (int id);
